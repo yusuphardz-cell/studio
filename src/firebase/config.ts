a27916475a61e78741616e572233238ebf2cd203
yusup hardiansyah <yusuph9@gmail.com>
2026-02-09 04:03:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9394776553-5896e",
+  "appId": "1:71581132819:web:ad9ba81708a0b36b31b7e6",
+  "apiKey": "AIzaSyAWqMNMaZieq9ILLSIcKWtEROxR2-hjP2o",
+  "authDomain": "studio-9394776553-5896e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "71581132819"
+};
